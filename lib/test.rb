b84f7252f3681eb4d3b51d3b23cb27450cdfa3da
shaouari <115957032+shaouari@users.noreply.github.com>
2022-12-02 15:54:32 +0100

    Update test.rb

--- lib/test.rb
+++ lib/test.rb
@@ -3,9 +3,9 @@ require "final_destination"
 class FinalDestination::Resolver
     module OpenIdResolverLookup
         def lookup(addr, timeout: nil)
-             Rails.logger.debug("======== OpenIdResolverLookup Override =========")
+            Rails.logger.debug("======== OpenIdResolverLookup Override =========")
             super(addr,20);                
         end
     end
-    singleton_class.prepend OpenIdResolverLookup
+    prepend OpenIdResolverLookup
 end
