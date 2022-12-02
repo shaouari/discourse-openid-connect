6bd9a98b9f03fc23e1e9e557b9750e3d58b52fd4
shaouari <115957032+shaouari@users.noreply.github.com>
2022-12-02 16:06:31 +0100

    Update test.rb

--- lib/test.rb
+++ lib/test.rb
@@ -3,8 +3,34 @@ require "final_destination"
 class FinalDestination::Resolver
     module OpenIdResolverLookup
         def lookup(addr, timeout: nil)
-            Rails.logger.info "======== OpenIdResolverLookup Override ========="
-            super(addr,20);                
+            Rails.logger.warn "======== OpenIdResolverLookup Override ========="
+            timeout = 1
+            @mutex.synchronize do
+              @result = nil
+
+              @queue ||= Queue.new
+              @queue << ""
+              ensure_lookup_thread
+
+              @lookup = addr
+              @parent = Thread.current
+
+              # This sleep will be interrupted by the lookup thread
+              # if completed within timeout
+              sleep timeout
+              if !@result
+                @thread.kill
+                @thread.join
+                @thread = nil
+                if @error
+                  @error.backtrace.push(*caller)
+                  raise @error
+                else
+                  raise Timeout::Error
+                end
+              end
+              @result
+            end               
         end
     end
     prepend OpenIdResolverLookup
