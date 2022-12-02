9d42fbc38487165d62b211551f325b66dea21c46
shaouari <115957032+shaouari@users.noreply.github.com>
2022-12-02 16:28:46 +0100

    Update test.rb

--- lib/test.rb
+++ lib/test.rb
@@ -1,37 +1,54 @@
 require "final_destination"
 
 class FinalDestination::Resolver
-    module OpenIdResolverLookup
-        def lookup(addr, timeout: nil)
-            Rails.logger.warn "======== OpenIdResolverLookup Override ========="
-            timeout = 1
-            @mutex.synchronize do
-              @result = nil
+  @mutex = Mutex.new
+  def self.lookup(addr, timeout: nil)
+    timeout ||= 2
+    @mutex.synchronize do
+      @result = nil
 
-              @queue ||= Queue.new
-              @queue << ""
-              ensure_lookup_thread
+      @queue ||= Queue.new
+      @queue << ""
+      ensure_lookup_thread
 
-              @lookup = addr
-              @parent = Thread.current
+      @lookup = addr
+      @parent = Thread.current
 
-              # This sleep will be interrupted by the lookup thread
-              # if completed within timeout
-              sleep timeout
-              if !@result
-                @thread.kill
-                @thread.join
-                @thread = nil
-                if @error
-                  @error.backtrace.push(*caller)
-                  raise @error
-                else
-                  raise Timeout::Error
-                end
-              end
-              @result
-            end               
+      # This sleep will be interrupted by the lookup thread
+      # if completed within timeout
+      sleep timeout
+      if !@result
+        @thread.kill
+        @thread.join
+        @thread = nil
+        if @error
+          @error.backtrace.push(*caller)
+          raise @error
+        else
+          raise Timeout::Error
         end
+      end
+      @result
     end
-    singleton_class.prepend OpenIdResolverLookup
+  end
+
+  private
+
+  def self.ensure_lookup_thread
+    return if @thread&.alive?
+
+    @thread = Thread.new do
+      while true
+        @queue.deq
+        @error = nil
+        begin
+          @result = Addrinfo.getaddrinfo(@lookup, 80, nil, :STREAM).map(&:ip_address)
+        rescue => e
+          @error = e
+        end
+        @parent.wakeup
+      end
+    end
+    @thread.name = "final-destination_resolver_thread"
+  end
 end
