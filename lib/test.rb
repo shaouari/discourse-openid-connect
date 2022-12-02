d65ab6cdbaa209711b380843abe9d7673ef8be26
shaouari <115957032+shaouari@users.noreply.github.com>
2022-12-02 16:39:53 +0100

    Update test.rb

--- lib/test.rb
+++ lib/test.rb
@@ -3,6 +3,7 @@ require "final_destination"
 class FinalDestination::Resolver
   @mutex = Mutex.new
   def self.lookup(addr, timeout: nil)
+    Rails.logger.warn "======== OpenIdResolverLookup Override ========="
     timeout = 1
     @mutex.synchronize do
       @result = nil
